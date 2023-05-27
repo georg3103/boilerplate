e3ca5ae24b77a2c4e85e6db92bba6d096a89202f
georg3103 <gnmkrtchyan@gmail.com>
2023-05-27 22:37:37 +0300

    fix(): test

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,7 +4,6 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from '.';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
